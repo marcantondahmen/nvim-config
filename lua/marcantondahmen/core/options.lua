f64f104ffafe5510e69bf446e7a8adf7563cdd96
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-19 12:02:01 +0100

    add basic options

---
+++ lua/marcantondahmen/core/options.lua
@@ -0,0 +1,40 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indents
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = 'dark'
+opt.signcolumn = 'yes'
+
+-- backspace
+opt.backspace = 'indent,eol,start'
+
+-- clipboard
+opt.clipboard:append('unnamedplus')
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- word char
+opt.iskeyword:append('-')
+
