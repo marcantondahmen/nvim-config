7079d3219e54c83afd3dcb3ee6f2f1fd8f0b8082
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-19 16:07:15 +0100

    add nvim-tree

---
+++ lua/marcantondahmen/plugins/nvim-tree.lua
@@ -0,0 +1,60 @@
+-- import nvim-tree plugin safely
+local setup, nvimtree = pcall(require, 'nvim-tree')
+if not setup then
+	return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
+-- configure nvim-tree
+nvimtree.setup({
+	-- change folder arrow icons
+	renderer = {
+		full_name = true,
+		--group_empty = true,
+		special_files = {},
+		symlink_destination = false,
+		indent_markers = {
+			enable = true,
+		},
+		icons = {
+			git_placement = 'signcolumn',
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = false,
+				git = true,
+			},
+		},
+	},
+	-- disable window_picker for
+	-- explorer to work well with
+	-- window splits
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	filters = {
+		custom = {
+			'^.git$',
+		},
+	},
+	git = {
+		ignore = false,
+	},
+})
+
+-- open nvim-tree on setup
+
+local function open_nvim_tree()
+	-- open the tree
+	require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
