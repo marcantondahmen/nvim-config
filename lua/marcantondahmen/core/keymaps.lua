7f62535493aed83fd90f512a9fcb0bafc4889614
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-20 20:35:48 +0100

    add trouble

--- lua/marcantondahmen/core/keymaps.lua
+++ lua/marcantondahmen/core/keymaps.lua
@@ -22,6 +22,14 @@ keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>') -- toggle split window max
 -- nvim-tree
 keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>') -- toggle file explorer
 
+-- trouble
+keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
+keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true, noremap = true })
+keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true, noremap = true })
+keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true })
+keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true })
+keymap.set('n', 'gR', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true })
+
 -- gitui
 keymap.set('n', '<leader>gu', '<cmd>Gitui<cr>')
 
