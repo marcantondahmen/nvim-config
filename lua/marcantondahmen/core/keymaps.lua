77607b98426f689925ecfb4a2d6a484d7cc3b5a1
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-21 10:33:14 +0100

    add keymaps for navigating between splits

--- lua/marcantondahmen/core/keymaps.lua
+++ lua/marcantondahmen/core/keymaps.lua
@@ -9,6 +9,11 @@ keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
 keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width & height
 keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
 
+keymap.set('n', '<c-k>', ':wincmd k<CR>', { silent = true, noremap = true })
+keymap.set('n', '<c-j>', ':wincmd j<CR>', { silent = true, noremap = true })
+keymap.set('n', '<c-h>', ':wincmd h<CR>', { silent = true, noremap = true })
+keymap.set('n', '<c-l>', ':wincmd l<CR>', { silent = true, noremap = true })
+
 keymap.set('n', '<leader>to', ':tabnew<CR>') -- open new tab
 keymap.set('n', '<leader>tx', ':tabclose<CR>') -- close current tab
 keymap.set('n', '<leader>tn', ':tabn<CR>') -- go to next tab
