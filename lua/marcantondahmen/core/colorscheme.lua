ed2b78666c13bb8d61f1aacac485bf9a750eaeda
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-22 17:54:48 +0100

    minor color scheme changes

--- lua/marcantondahmen/core/colorscheme.lua
+++ lua/marcantondahmen/core/colorscheme.lua
@@ -7,3 +7,23 @@ vim.g.nord_bold = false
 
 -- Load the colorscheme
 require('nord').set()
+
+-- https://github.com/shaunsingh/nord.nvim/blob/master/lua/nord/theme.lua
+vim.cmd([[
+	colorscheme nord
+
+	highlight FloatBorder guifg=#4C566A
+	highlight TelescopePromptBorder guifg=#4C566A
+	highlight TelescopeResultsBorder guifg=#4C566A
+	highlight TelescopePreviewBorder guifg=#4C566A
+	highlight NvimTreeIndentMarker guifg=#4C566A
+	highlight LspFloatWinBorder guifg=#4C566A
+	highlight LspSagaDefPreviewBorder guifg=#4C566A
+	highlight LspSagaCodeActionBorder guifg=#4C566A
+	highlight LspSagaLspFinderBorder guifg=#4C566A
+	highlight DefinitionBorder guifg=#4C566A
+	highlight LspSagaHoverBorder guifg=#4C566A
+	highlight LspSagaRenameBorder guifg=#4C566A
+	highlight LspSagaDiagnosticBorder guifg=#4C566A
+	highlight LSOutlinePreviewBorder guifg=#4C566A
+]])
