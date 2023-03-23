32fec9c5817051834ae33640efec59c6bef8d22f
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-23 15:41:25 +0100

    disable word diff

--- lua/marcantondahmen/plugins/gitsigns.lua
+++ lua/marcantondahmen/plugins/gitsigns.lua
@@ -8,7 +8,7 @@ end
 gitsigns.setup({
 	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 })
