a8020a8395b6f3d3003d137ad9364725d1913327
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-23 09:30:20 +0100

    improve gitsigns config

--- lua/marcantondahmen/plugins/gitsigns.lua
+++ lua/marcantondahmen/plugins/gitsigns.lua
@@ -5,4 +5,10 @@ if not setup then
 end
 
 -- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup({
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+})
