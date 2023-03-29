e0b730ed796bfc70e39ef13cd22cb6da6d0fe8cc
Marc Anton Dahmen <mail@marcdahmen.de>
2023-03-29 17:21:40 +0200

    add prismals

--- lua/marcantondahmen/plugins/lsp/mason.lua
+++ lua/marcantondahmen/plugins/lsp/mason.lua
@@ -30,6 +30,7 @@ mason_lspconfig.setup({
 		'emmet_ls',
 		'intelephense',
 		'psalm',
+		'prismals',
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
