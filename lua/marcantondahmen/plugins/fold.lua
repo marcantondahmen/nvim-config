8a1c7257af40aca5ff8ca2c40f3838c1cf1c19a2
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-01 12:42:14 +0200

    improve folding config

--- lua/marcantondahmen/plugins/fold.lua
+++ lua/marcantondahmen/plugins/fold.lua
@@ -5,12 +5,12 @@ opt.foldexpr = 'nvim_treesitter#foldexpr()'
 opt.fdc = '0'
 
 local function init_fold()
-	vim.fn.timer_start(20, function()
+	vim.fn.timer_start(5, function()
 		vim.cmd([[normal zx zR]])
 	end)
 end
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-	pattern = { '*' },
+vim.api.nvim_create_autocmd({ 'BufReadPost, FileReadPost' }, {
+	pattern = { '*.*' },
 	callback = init_fold,
 })
