1ec08345bf13590edca2098e0de50f1db4577f26
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-02 15:57:05 +0200

    make self-update run as background job

--- lua/marcantondahmen/plugins/self-update.lua
+++ lua/marcantondahmen/plugins/self-update.lua
@@ -1,24 +1,33 @@
-local config = vim.fn.stdpath('config')
+local function gitCmdStr(cmd)
+	local config = vim.fn.stdpath('config')
+	return 'git --git-dir=' .. config .. '/.git --work-tree=' .. config .. ' ' .. cmd .. ' 2>&1'
+end
 
-local function git(dir, cmd)
-	local handle = io.popen('git --git-dir=' .. dir .. '/.git --work-tree=' .. dir .. ' ' .. cmd .. ' 2>&1')
-	local output = handle:read('*a')
+local function pullCallback(id, output, name)
+	vim.notify(table.concat(output, '\n'))
+	vim.fn.timer_start(500, function()
+		require('packer').sync()
+	end)
+end
 
-	handle:close()
+local function fetchCallback(id, output, name)
+	local status = table.concat(output, ' ')
 
-	return output
+	if status == '' then
+		vim.fn.timer_start(500, function()
+			vim.fn.jobstart(gitCmdStr('pull'), {
+				stdout_buffered = true,
+				on_stdout = pullCallback,
+			})
+		end)
+	end
 end
 
 local function checkForUpdates()
-	local status = git(config, 'fetch origin --dry-run')
-
-	if status ~= '' then
-		vim.fn.timer_start(500, function()
-			local pull = git(config, 'pull')
-			vim.notify(pull, 'info')
-			require('packer').sync()
-		end)
-	end
+	vim.fn.jobstart(gitCmdStr('fetch origin --dry-run'), {
+		stdout_buffered = true,
+		on_stdout = fetchCallback,
+	})
 end
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = checkForUpdates })
