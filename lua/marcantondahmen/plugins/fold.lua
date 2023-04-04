eb379cc62fe1c7d8325b2c76049e7d4bfdd238db
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-04 11:44:00 +0200

    use treesitter again for folding and prevent initial fold

--- lua/marcantondahmen/plugins/fold.lua
+++ lua/marcantondahmen/plugins/fold.lua
@@ -1,11 +1,13 @@
 -- use treesitter for folding
 local opt = vim.opt
-opt.foldmethod = 'indent'
+opt.foldmethod = 'expr'
+opt.foldexpr = 'nvim_treesitter#foldexpr()'
+opt.foldlevel = 99
 opt.fdc = '0'
 
 local function init_fold()
 	vim.fn.timer_start(5, function()
-		vim.cmd([[normal zR]])
+		vim.cmd([[normal zx]])
 	end)
 end
 
