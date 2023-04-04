e7f967dba3cac2d914a59b5e502d33e12b960ff8
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-04 14:47:23 +0200

    change foldmethod back again to indent

--- lua/marcantondahmen/plugins/fold.lua
+++ lua/marcantondahmen/plugins/fold.lua
@@ -1,17 +1,5 @@
 -- use treesitter for folding
 local opt = vim.opt
-opt.foldmethod = 'expr'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
+opt.foldmethod = 'indent'
 opt.foldlevel = 99
 opt.fdc = '0'
-
-local function init_fold()
-	vim.fn.timer_start(5, function()
-		vim.cmd([[normal zx]])
-	end)
-end
-
-vim.api.nvim_create_autocmd({ 'BufReadPost, FileReadPost' }, {
-	pattern = { '*.*' },
-	callback = init_fold,
-})
