d6133630785fe93d041f1e8566e8dd2dc1acd4d8
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-17 16:18:53 +0200

    improve notify setup

--- lua/marcantondahmen/plugins/notify.lua
+++ lua/marcantondahmen/plugins/notify.lua
@@ -1,1 +1,13 @@
-vim.notify = require('notify')
+local status, notify = pcall(require, 'notify')
+if not status then
+	return
+end
+
+notify.setup({
+	stages = 'static',
+	render = 'default',
+	max_width = 60,
+	min_width = 60,
+})
+
+vim.notify = notify
