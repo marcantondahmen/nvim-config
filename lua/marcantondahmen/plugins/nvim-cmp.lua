52d26c9f7ad403d8daaa153189ad7cf2ff28b08e
Marc Anton Dahmen <mail@marcdahmen.de>
2023-04-18 11:02:41 +0200

    fix autocompletion using tab

--- lua/marcantondahmen/plugins/nvim-cmp.lua
+++ lua/marcantondahmen/plugins/nvim-cmp.lua
@@ -27,6 +27,12 @@ require('luasnip/loaders/from_vscode').lazy_load()
 
 vim.opt.completeopt = 'menu,menuone,noselect'
 
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -42,14 +48,16 @@ cmp.setup({
 		['<C-e>'] = cmp.mapping.abort(), -- close completion window
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
 		['<Tab>'] = cmp.mapping(function(fallback)
-			if luasnip.expandable() then
-				luasnip.expand()
-			elseif cmp.visible() then
+			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
 			elseif luasnip.jumpable(1) then
 				luasnip.jump(1)
 			elseif neogen.jumpable() then
 				neogen.jump_next()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
