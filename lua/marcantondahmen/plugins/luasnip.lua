395eaba67e9d0bd9f70258d9f0d0b60d16a97601
Marc Anton Dahmen <mail@marcdahmen.de>
2023-05-06 18:05:08 +0200

    add snippets

--- lua/marcantondahmen/plugins/luasnip.lua
+++ lua/marcantondahmen/plugins/luasnip.lua
@@ -3,16 +3,25 @@ if not status then
 	return
 end
 
+local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
 
 luasnip.add_snippets('all', {
-	luasnip.snippet({ trig = '/**', name = 'Doc Block', dscr = 'Add a doc block comment' }, {
+	s({ trig = '/**', name = 'Doc Block', dscr = 'Add a doc block comment' }, {
 		t({ '/**' }),
 		t({ '', ' * ' }),
 		i(1),
 		t({ '', ' */' }),
 	}),
+})
+
+luasnip.add_snippets('typescript', {
+	s(
+		{ trig = 'class', name = 'HTML class attribute', dscr = 'Add class attribute', priority = 2000 },
+		{ t('class="'), i(1), t('"') }
+	),
+	s({ trig = 'clg', name = 'console.log()', dscr = 'Add "console.log()"' }, { t('console.log('), i(1), t(')') }),
 })
 
 vim.api.nvim_create_autocmd('ModeChanged', {
