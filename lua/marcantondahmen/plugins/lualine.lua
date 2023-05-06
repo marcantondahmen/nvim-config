a503df1cf7367bdb3647e279fffac06a59603eab
Marc Anton Dahmen <mail@marcdahmen.de>
2023-05-06 18:04:51 +0200

    improve lualine config

--- lua/marcantondahmen/plugins/lualine.lua
+++ lua/marcantondahmen/plugins/lualine.lua
@@ -15,14 +15,26 @@ lualine.setup({
 			statusline = { 'alpha' },
 			winbar = {},
 		},
+		ignore_focus = {
+			'NvimTree',
+		},
 	},
 	sections = {
 		lualine_c = {
 			{
 				'filename',
 				file_status = true, -- displays file status (readonly status, modified status)
-				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				path = 4, -- 0 = just filename, 1 = relative path, 2 = absolute path, 3: Absolute path, with tilde as the home directory, 4: Filename and parent dir, with tilde as the home directory
+				symbols = {
+					modified = '●', -- Text to show when the file is modified.
+					readonly = '󰍶', -- Text to show when the file is non-modifiable or readonly.
+					unnamed = '', -- Text to show for unnamed buffers.
+					newfile = 'New', -- Text to show for newly created file before first write
+				},
 			},
 		},
+		lualine_x = { 'filetype' },
+		lualine_y = { 'string.gsub(vim.fn.getcwd(), "(.*/)(.*)", "%2")' },
+		lualine_z = { 'progress' },
 	},
 })
