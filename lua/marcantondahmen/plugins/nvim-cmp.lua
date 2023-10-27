53498f35f3d561aa48276b18a9d40b90acf75dc8
Marc Anton Dahmen <mail@marcdahmen.de>
2023-10-27 14:48:27 +0200

    add back tab completion

--- lua/marcantondahmen/plugins/nvim-cmp.lua
+++ lua/marcantondahmen/plugins/nvim-cmp.lua
@@ -16,6 +16,18 @@ if not lspkind_status then
 	return
 end
 
+-- import neogen plugin safely
+local neogen_status, neogen = pcall(require, 'neogen')
+if not neogen_status then
+	return
+end
+
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require('luasnip/loaders/from_vscode').lazy_load()
 
@@ -34,7 +46,34 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
 		['<C-e>'] = cmp.mapping.abort(), -- close completion window
+		['<Esc>'] = cmp.mapping.abort(), -- close completion window
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
+			elseif luasnip.jumpable(1) then
+				luasnip.jump(1)
+			elseif neogen.jumpable() then
+				neogen.jump_next()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			elseif neogen.jumpable(true) then
+				neogen.jump_prev()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({
