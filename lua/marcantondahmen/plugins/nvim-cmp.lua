8873bd4f4e8069e9280471af7eca7eb01013ce66
Marc Anton Dahmen <mail@marcdahmen.de>
2023-10-27 18:47:03 +0200

    update cmp keymaps

--- lua/marcantondahmen/plugins/nvim-cmp.lua
+++ lua/marcantondahmen/plugins/nvim-cmp.lua
@@ -55,7 +55,6 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
 		['<C-e>'] = cmp.mapping.abort(), -- close completion window
-		['<Esc>'] = cmp.mapping.abort(), -- close completion window
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
 		['<Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
