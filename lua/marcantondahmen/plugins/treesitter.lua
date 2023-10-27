abef35812bc31fe138a84e3f176e33b791fd7ce2
Marc Anton Dahmen <mail@marcdahmen.de>
2023-10-27 11:40:22 +0200

    add tree-sitter-automad

--- lua/marcantondahmen/plugins/treesitter.lua
+++ lua/marcantondahmen/plugins/treesitter.lua
@@ -4,6 +4,14 @@ if not status then
 	return
 end
 
+local parsers = require('nvim-treesitter.parsers').get_parser_configs()
+parsers.automad = {
+	install_info = {
+		url = 'https://github.com/automadcms/tree-sitter-automad', -- local path or git repo
+		files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+	},
+}
+
 -- configure treesitter
 treesitter.setup({
 	-- enable syntax highlighting
@@ -17,6 +25,7 @@ treesitter.setup({
 	autotag = { enable = true, enable_close_on_slash = false },
 	-- ensure these language parsers are installed
 	ensure_installed = {
+		'automad',
 		'json',
 		'javascript',
 		'typescript',
