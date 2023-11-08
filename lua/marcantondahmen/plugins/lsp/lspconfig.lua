7b387725368036c15bb9d11101c1f135244e241b
Marc Anton Dahmen <mail@marcdahmen.de>
2023-11-08 17:46:31 +0100

    fix tailwind css warnings

--- lua/marcantondahmen/plugins/lsp/lspconfig.lua
+++ lua/marcantondahmen/plugins/lsp/lspconfig.lua
@@ -83,6 +83,17 @@ tsTools.setup({
 lspconfig['cssls'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
+	settings = {
+		css = { validate = true, lint = {
+			unknownAtRules = 'ignore',
+		} },
+		scss = { validate = true, lint = {
+			unknownAtRules = 'ignore',
+		} },
+		less = { validate = true, lint = {
+			unknownAtRules = 'ignore',
+		} },
+	},
 })
 
 -- configure tailwindcss server
