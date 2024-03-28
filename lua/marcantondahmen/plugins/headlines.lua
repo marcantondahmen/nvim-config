8fc121a25b38001bcb739dbe50e94fa73ae582de
Marc Anton Dahmen <mail@marcdahmen.de>
2024-03-28 21:50:04 +0100

    disable headlines plugin in insert mode

--- lua/marcantondahmen/plugins/headlines.lua
+++ lua/marcantondahmen/plugins/headlines.lua
@@ -9,5 +9,20 @@ return {
 		end
 
 		headlines.setup()
+
+		-- Toggle markdown headlines on insert enter/leave
+		-- see https://github.com/LazyVim/LazyVim/commit/0ba731a87977e98f2420e2c6d2c4cabbcedaff60
+		local md = headlines.config.markdown
+
+		vim.api.nvim_create_augroup('madHeadlines', { clear = true })
+		vim.api.nvim_create_autocmd({ 'InsertEnter', 'InsertLeave' }, {
+			group = 'madHeadlines',
+			callback = function(data)
+				if vim.bo.filetype == 'markdown' then
+					headlines.config.markdown = data.event == 'InsertLeave' and md or nil
+					headlines.refresh()
+				end
+			end,
+		})
 	end,
 }
