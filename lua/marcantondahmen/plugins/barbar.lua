1cf1763c9e18d41d316b13e21bad0fec9f2e5a72
Marc Anton Dahmen <mail@marcdahmen.de>
2024-10-10 20:19:37 +0200

    disable auto hide in barbar config

--- lua/marcantondahmen/plugins/barbar.lua
+++ lua/marcantondahmen/plugins/barbar.lua
@@ -11,7 +11,7 @@ return {
 			animation = true,
 
 			-- Enable/disable auto-hiding the tab bar when there is a single buffer
-			auto_hide = true,
+			auto_hide = false,
 
 			-- Enable/disable current/total tabpages indicator (top right corner)
 			tabpages = false,
